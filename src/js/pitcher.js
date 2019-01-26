2b1afc714cc8b3fecfd71deea984507cf07428f9
Robert Moore <robert.ben.moore@gmail.com>
2019-01-26 12:01:27 -0800

    test
    test

--- src/js/pitcher.js
+++ src/js/pitcher.js
@@ -4,10 +4,23 @@ export default class Pitcher extends Sprite{
 	constructor(game){
 		super(game);
 		console.log("I'm a pitcher");
-
+		this.game = game;
+			
 			/* For calculating ball position */
 			this.throwSpeed = null;
+
+
+			/* For calculating where the pitch is*/
+			this.pitchStart = null;
+			this.pitchSpeed = null
+
 	}
+	throwPitch(){
+		this.pitchStart = this.game.roundTime;
+		this.pitchSpeed = 10;
+		
+	}
+	
 }
-/* Throws a pitch */
+
 
